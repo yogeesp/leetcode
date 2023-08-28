dd1b84645ea578bc6e04a8aa6ac9280a66271ac5
yogeesp <72245013+yogeesp@users.noreply.github.com>
2023-08-28 17:58:33 +0530

    Time: 25 ms (43.83%), Space: 31.1 MB (83.02%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+        int sumi = 0;
+        
+        int leftSum = 0;
+        
+        for(int i =0;i<nums.size();i++){
+            sumi+=nums[i];
+        }
+        
+        for(int i = 0;i<nums.size();i++){
+            int totalsum = sumi-leftSum-nums[i];
+            
+            if(leftSum == totalsum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
+};
